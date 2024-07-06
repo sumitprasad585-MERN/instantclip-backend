1fe5007dd78a537885040d033d4a6d5f3f2e44b9
Sumit Prasad <sumitprasad585@gmail.com>
2024-07-06 11:48:29 +0100

    handle errors for production environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/AppError");
+
 const sendErrorForDevelopmentEnv = (err, res) => {
   const statusCode = err.statusCode || 500;
   const status = err.status || 'error';
@@ -11,7 +13,10 @@ const sendErrorForDevelopmentEnv = (err, res) => {
 
 const sendErrorForProductionEnv = (err, res) => {
   if (err.isOperationalError) {
-
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
   } else {
     res.status(500).json({
       status: 'error',
@@ -24,7 +29,28 @@ const globalErrorHandler = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorForDevelopmentEnv(err, res);
   } else {
-    sendErrorForProductionEnv(err, res);
+    let error = Object.create(err);
+
+    /** Handle cast error */
+    if (error.name === 'CastError') {
+      const message = 'Invalid ID :: ' + error.value;
+      error = new AppError(400, message);
+    }
+
+    /** Handle duplicate value error */
+    if (error.code === 11000) {
+      const duplicateKeyValue = Object.entries(error.keyValue)[0];
+      const message = `Duplicate value for field ${duplicateKeyValue[0]} :: ${duplicateKeyValue[1]}. Already present in DB`;
+      error = new AppError(400, message);
+    }
+
+    /** Handle Validation Error */
+    if (error.name === 'ValidationError') {
+      const message = Object.values(error.errors).map(current => current.message).join('. ');
+      error = new AppError(400, message);
+    }
+
+    sendErrorForProductionEnv(error, res);
   }
 };
 
