b2cfcb527e443c8fe6f0fc135acb76d72539c4ab
Sumit Prasad <sumitprasad585@gmail.com>
2025-01-06 20:26:19 +0000

    connect to db

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: ['./config.env', './config.private.env'] });
+const mongoose = require('mongoose');
 const app = require('./app');
+
+let DATABASE = process.env.DATABASE_URL;
+DATABASE = DATABASE.replace('<db_username>', process.env.DATABASE_USERNAME);
+DATABASE = DATABASE.replace('<db_password>', process.env.DATABASE_PASSWORD);
+DATABASE = DATABASE.replace('<db_name>', process.env.DBNAME);
+
+mongoose
+  .connect(DATABASE)
+  .then((con) => {
+    // console.log(con);
+    console.log('Database connection successful✅');
+  })
+  .catch((err) => {
+    console.error(err);
+    console.log('Shutting down the app...');
+    process.exit(1);
+  });
 
 const PORT = 8888;
 const server = app.listen(PORT, () => {
@@ -22,4 +40,3 @@ server.on('uncaughtException', (err) => {
   console.log('Shutting down the app...');
   process.exit(1);
 });
-
