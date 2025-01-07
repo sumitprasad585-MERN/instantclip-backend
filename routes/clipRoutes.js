50b9210f778c2de8d73347109b19c8dc1297f36f
Sumit Prasad <sumitprasad585@gmail.com>
2025-01-07 13:55:43 +0000

    Protected routes for clips API

--- routes/clipRoutes.js
+++ routes/clipRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const Clip = require('../models/clipModel');
 const { getAllClips, createNewClip, updateClip, deleteClip } = require('../controllers/clipController');
+const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.get('/', getAllClips);
+router.get('/', protect, getAllClips);
 
-router.post('/', createNewClip);
+router.post('/', protect, createNewClip);
 
-router.patch('/:id', updateClip);
+router.patch('/:id', protect, updateClip);
 
-router.delete('/:id', deleteClip);
+router.delete('/:id', protect, deleteClip);
 
 module.exports = router;
